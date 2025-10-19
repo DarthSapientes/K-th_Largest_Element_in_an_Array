fdbf72fa596ba0c30abb0ee352c3b6d8df323d52
DarthSapientes <91483533+DarthSapientes@users.noreply.github.com>
2025-10-19 22:43:29 +0530

    Update Quickselect.py

--- Quickselect.py
+++ Quickselect.py
@@ -17,8 +17,24 @@ def quickselect(A, k = int):
     else:
         return quickselect(right, ((k - len(left)) - 1))
 
+def randomized_quickselect(A, k = int): 
+    # A variation of the quickselect algorithm with randomized pivot selection
+    if len(A) == 1:
+        return A[0]
+
+    pivot = A[random.randint(1, len(A)-1)] # random selection of pivot
+    left = [x for x in A if x > pivot]  # elements larger than pivot
+    right = [x for x in A if x <= pivot] # elements smaller or to equal
+    right.remove(pivot) #removes an instance of the pivot from ‘right’
+    if k <= len(left):
+        return randomized_quickselect(left, k)
+    elif k == len(left) + 1:
+        return pivot
+    else:
+        return randomized_quickselect(right, ((k - len(left)) - 1))
 
 A = [81, 2, 25, 33, 69, 39, 42, 17, 62, 15]
 k = 5
 result = quickselect(A, k)
 print(f"The {k}-th largest element is {result}")
+
